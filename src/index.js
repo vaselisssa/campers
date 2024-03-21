795a6f2b9277e1f579b4f4c8311d5a3dbd9c4058
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-03-21 06:23:28 +0200

    fix routes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <BrowserRouter>
+   <BrowserRouter basename="/campers">
       <React.StrictMode>
          <App />
       </React.StrictMode>
