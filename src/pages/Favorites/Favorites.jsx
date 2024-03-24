d2cd748ccdfef96410c8293bf9a9c0053362b87d
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-03-24 17:38:39 +0200

    add Favorites page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectFavorites } from "../../redux/adverts/selectors";
+import AdvertItem from "../../components/AdvertItem";
+import { FavListStyled, EmptyFav } from "./Favorites.styled";
 
 const Favorites = () => {
-   return <div>Favorites</div>;
+   const favorites = useSelector(selectFavorites);
+   return (
+      <>
+         {favorites.length > 0 ? (
+            <FavListStyled>
+               {favorites.map((el) => (
+                  <AdvertItem key={el._id} item={el} />
+               ))}
+            </FavListStyled>
+         ) : (
+            <EmptyFav>
+               Looks like your favorites list is empty. Let's add some
+               favorites!
+            </EmptyFav>
+         )}
+      </>
+   );
 };
 
 export default Favorites;
---
+++ src/pages/Favorites/Favorites.styled.jsx
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+export const FavListStyled = styled.div`
+   display: flex;
+   flex-direction: column;
+   gap: 24px;
+`;
+
+export const EmptyFav = styled.div`
+   font-size: 40px;
+   font-weight: 600;
+   line-height: 1.25;
+   text-align: center;
+   color: var(--button-color);
+   margin-top: 50px;
+
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   gap: 10px;
+`;
