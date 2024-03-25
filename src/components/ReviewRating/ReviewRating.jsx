f43102605592dfd307dd99e0c295861a8911e32b
Svitlana Vasylkovska <vaselisssa@gmail.com>
2024-03-25 22:20:00 +0200

    add raiting stars

--- src/components/ReviewRating/ReviewRating.jsx
+++ src/components/ReviewRating/ReviewRating.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Icon from "../Icon";
+import { RatingStyled, FilledStar, EmptyStar } from "./ReviewRating.styled";
 
 const ReviewRating = ({ rating }) => {
-   return <div>{rating}</div>;
+   const stars = 5;
+
+   return (
+      <RatingStyled>
+         {[...Array(stars)].map((_, index) => {
+            const filled = index < rating;
+            return filled ? (
+               <FilledStar key={index}>
+                  <Icon icon="icon-star" width={16} height={16} />
+               </FilledStar>
+            ) : (
+               <EmptyStar key={index}>
+                  <Icon icon="icon-star" width={16} height={16} />
+               </EmptyStar>
+            );
+         })}
+      </RatingStyled>
+   );
 };
 
 export default ReviewRating;
--- src/components/ReviewRating/ReviewRating.styled.jsx
+++ src/components/ReviewRating/ReviewRating.styled.jsx
@@ -1,1 +1,18 @@
 import styled from "@emotion/styled";
+
+export const RatingStyled = styled.div`
+   display: flex;
+   gap: 4px;
+`;
+
+export const FilledStar = styled.div`
+   svg {
+      fill: var(--rating-color);
+   }
+`;
+
+export const EmptyStar = styled.div`
+   svg {
+      fill: var(--feature-background-color);
+   }
+`;
